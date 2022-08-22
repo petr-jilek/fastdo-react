4c20bac1fdb5cfeeb574a749df6e0dfbeabaa6d4
Petr Jílek <petrjilek@centrum.cz>
2022-08-22 16:40:07 +0200

    Edit code

--- src/react-components/components/general/HrSpacer.tsx
+++ src/react-components/components/general/HrSpacer.tsx
@@ -4,5 +4,5 @@ export interface Props {
 
 export default function HrSpacer({ height = 1 }: Props) {
 
-    return <div style={{ backgroundColor: "var(--primary-black-color)", height: `${height}px` }}></div>
+    return <div style={{ backgroundColor: "var(--primary-black-color)", height: `${height}px`, width: "100%" }}></div>
 }
