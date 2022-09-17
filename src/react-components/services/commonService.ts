07ce6b0f3c8327337656187496030f0e95f37f6c
Petr Jilek <petrjilek@centrum.cz>
2022-09-17 19:59:26 +0200

    Add common methods

--- src/react-components/services/commonService.ts
+++ src/react-components/services/commonService.ts
@@ -1,1 +1,9 @@
 export const isTypeOf = () => true
+
+export const splitNumberBy3Digits = (value: number) => value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+
+export const appendToStringWithSpace = (value: string, append: string) => value + " " + append
+
+export const appendKc = (value: string) => appendToStringWithSpace(value, " Kč")
+
+export const toCzCurrencyString = (value: number) => appendKc(splitNumberBy3Digits(value))
