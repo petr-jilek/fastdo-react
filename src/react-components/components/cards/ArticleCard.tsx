837983d43ddccf0138c4cf9b46af130df186a465
Petr Jilek <petrjilek@centrum.cz>
2022-09-17 14:48:13 +0200

    Edit styles

--- src/react-components/components/cards/ArticleCard.tsx
+++ src/react-components/components/cards/ArticleCard.tsx
@@ -1,42 +1,26 @@
-import styles from "./ArticleCard.module.css";
+import styles from "./ArticleCard.module.css"
 
 export interface Props {
-  id: string;
-  name: string;
-  description: string;
-  imageName: string;
-  getImageUrl: string;
-  note?: string;
-  imageOnClick?: (id: string) => void;
+  id: string
+  name: string
+  description: string
+  imageName: string
+  getImageUrl: string
+  note?: string
+  imageOnClick?: (id: string) => void
 }
 
-export default function ArticleCard({
-  id,
-  name,
-  description,
-  imageName,
-  getImageUrl,
-  note = "",
-  imageOnClick = () => {},
-}: Props) {
+export default function ArticleCard({ id, name, description, imageName, getImageUrl, note = "", imageOnClick = () => {} }: Props) {
   return (
-    <div className={styles.component}>
-      <div
-        className={styles.imageDiv}
-        style={{ backgroundImage: `url(${getImageUrl + "/" + imageName})` }}
-        onClick={() => imageOnClick(id)}
-      >
-        <img
-          src={getImageUrl + "/" + imageName}
-          alt="img"
-          onClick={() => imageOnClick(id)}
-        />
+    <div className={styles.component} onClick={() => imageOnClick(id)}>
+      <div className={styles.imageDiv} style={{ backgroundImage: `url(${getImageUrl + "/" + imageName})` }}>
+        <img src={getImageUrl + "/" + imageName} alt="img" />
       </div>
       <div className={styles.textDiv}>
-        <h3 onClick={() => imageOnClick(id)}>{name}</h3>
+        <h3>{name}</h3>
         {note !== "" ? <p className={styles.noteP}>{note}</p> : <></>}
         {description !== "" ? <p>{description}</p> : <></>}
       </div>
     </div>
-  );
+  )
 }
