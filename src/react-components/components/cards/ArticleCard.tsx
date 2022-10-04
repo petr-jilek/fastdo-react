5410dfe006ffd5fa83ee92ab62fa22f7bbabba8f
Petr Jilek <petrjilek@centrum.cz>
2022-10-04 08:26:59 +0200

    Edit code

--- src/react-components/components/cards/ArticleCard.tsx
+++ src/react-components/components/cards/ArticleCard.tsx
@@ -1,34 +1,28 @@
+import { Link } from "react-router-dom"
 import styles from "./ArticleCard.module.css"
 
 export interface Props {
   id: string
   name: string
-  description: string
   imageName: string
   getImageUrl: string
+  description?: string
   note?: string
-  imageOnClick?: (id: string) => void
 }
 
-export default function ArticleCard({
-  id,
-  name,
-  description,
-  imageName,
-  getImageUrl,
-  note = "",
-  imageOnClick = () => {},
-}: Props) {
+export default function ArticleCard({ id, name, imageName, getImageUrl, description = "", note = "" }: Props) {
   return (
-    <div className={styles.component} onClick={() => imageOnClick(id)}>
-      <div className={styles.imageDiv}>
-        <img src={getImageUrl + "/" + imageName} alt={imageName} loading="lazy" title={name} />
+    <Link to={"/articles/" + id} className={styles.wrapper}>
+      <div className={styles.component}>
+        <div className={styles.imageDiv}>
+          <img src={getImageUrl + "/" + imageName} alt={imageName} loading="lazy" title={name} />
+        </div>
+        <div className={styles.textDiv}>
+          <h3>{name}</h3>
+          {note !== "" ? <p className={styles.noteP}>{note}</p> : <></>}
+          {description !== "" ? <p>{description}</p> : <></>}
+        </div>
       </div>
-      <div className={styles.textDiv}>
-        <h3>{name}</h3>
-        {note !== "" ? <p className={styles.noteP}>{note}</p> : <></>}
-        {description !== "" ? <p>{description}</p> : <></>}
-      </div>
-    </div>
+    </Link>
   )
 }
