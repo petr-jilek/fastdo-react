d13ffd408df7b49539a918fa260208a3ed94e18d
Petr Jilek <petrjilek@centrum.cz>
2022-11-10 22:21:16 +0100

    Edit code

---
+++ src/components/form/text/FormaterTextField.tsx
@@ -0,0 +1,50 @@
+import { useRef, useState } from "react"
+import { removeAllWhitespaces, splitNumberBy3Digits } from "../../../services/commonService"
+import TextField, { IOnTextChangeData } from "./TextField"
+import styles from "./TextField.module.css"
+
+export interface Props {
+  id?: string
+  label?: string
+  placeholder?: string
+  value: string
+  onTextChange: ({ e, value }: IOnTextChangeData) => void
+  onEnter?: ({ e, value }: IOnTextChangeData) => void
+  divStyle?: React.CSSProperties
+  labelStyle?: React.CSSProperties
+  inputStyle?: React.CSSProperties
+  formater: (value: any) => any
+  inverseFormater: (value: any) => any
+}
+
+export default function FormaterTextField({
+  id = "",
+  label = "",
+  placeholder = "",
+  value,
+  onTextChange,
+  onEnter = () => {},
+  divStyle = {},
+  labelStyle = {},
+  inputStyle = {},
+  formater,
+  inverseFormater,
+}: Props) {
+  const onChange = ({ e, value }: IOnTextChangeData) => {
+    onTextChange({ e: e, value: inverseFormater(value) })
+  }
+
+  return (
+    <TextField
+      id={id}
+      label={label}
+      placeholder={placeholder}
+      value={formater(value)}
+      onTextChange={onChange}
+      onEnter={onEnter}
+      divStyle={divStyle}
+      labelStyle={labelStyle}
+      inputStyle={inputStyle}
+    />
+  )
+}
