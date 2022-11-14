d7fe3fe834aa75569ebcf6f2d58b85da77631398
Petr Jilek <petrjilek@centrum.cz>
2022-11-14 19:22:33 +0100

    Edit ocde

---
+++ src/components/general/Expandable.tsx
@@ -0,0 +1,29 @@
+import styles from "./Expandable.module.css"
+import { useState } from "react"
+import Card from "../cards/Card"
+import { BsChevronDown, BsChevronUp } from "react-icons/bs"
+
+export interface Props {
+  title: string
+  children: JSX.Element[] | JSX.Element
+}
+
+export default function Expandable({ title, children }: Props) {
+  const [expanded, setExpanded] = useState(false)
+
+  return (
+    <Card style={{ backgroundColor: expanded ? "#FFFFFF" : "#EFEFEF", transition: ".3s" }}>
+      <div className={styles.componentDiv}>
+        <div className={styles.headerDiv}>
+          <p className={styles.headerText}>{title}</p>
+          {expanded ? (
+            <BsChevronUp className={styles.expandIcon} onClick={() => setExpanded(false)} />
+          ) : (
+            <BsChevronDown className={styles.expandIcon} onClick={() => setExpanded(true)} />
+          )}
+        </div>
+        {expanded && <div className={styles.childrenDiv}>{children}</div>}
+      </div>
+    </Card>
+  )
+}
