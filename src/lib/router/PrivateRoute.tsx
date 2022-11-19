f223dc7870bea27b0f09a465fbdc71c496bc5716
Petr Jilek <petrjilek@centrum.cz>
2022-11-19 11:17:04 +0100

    Fix bug

--- src/lib/router/PrivateRoute.tsx
+++ src/lib/router/PrivateRoute.tsx
@@ -1,13 +1,12 @@
-import { Navigate, RouteProps } from "react-router-dom";
+import { Navigate, RouteProps } from "react-router-dom"
 
-interface Props extends RouteProps {
-    children: JSX.Element;
+type Props = RouteProps & {
+  children: JSX.Element
 }
 
 export default function PrivateRoute({ children }: Props) {
-    const token = window.localStorage.getItem('identity_token')
+  const token = window.localStorage.getItem("identity_token")
 
-    if (!token)
-        return <Navigate to="/404" />
-    return children
+  if (!token) return <Navigate to="/404" />
+  return children
 }
