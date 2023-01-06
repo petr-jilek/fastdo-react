f87d91ebe097fc0353e821c78c081f8b44201342
Petr Jilek <petr.jilek156@gmail.com>
2023-01-06 13:16:55 +0100

    Edit code

--- src/components/progressers/VerticalStepper.tsx
+++ src/components/progressers/VerticalStepper.tsx
@@ -7,19 +7,58 @@ export interface Props {
 
 export interface StepperItem {
   label: string
-  isDone: boolean
+  status: StepperItemStatus
+}
+
+export enum StepperItemStatus {
+  todo = 0,
+  focused = 1,
+  done = 2,
 }
 
 export default function VerticalStepper({ items }: Props) {
+  const getCircleClass = (status: StepperItemStatus) => {
+    switch (status) {
+      case StepperItemStatus.todo:
+        return styles.circle
+      case StepperItemStatus.focused:
+        return styles.circleFocused
+      case StepperItemStatus.done:
+        return styles.circleDone
+    }
+  }
+
+  const getLabelClass = (status: StepperItemStatus) => {
+    switch (status) {
+      case StepperItemStatus.todo:
+        return styles.label
+      case StepperItemStatus.focused:
+        return styles.labelDone
+      case StepperItemStatus.done:
+        return styles.labelDone
+    }
+  }
+
+  const getLineElement = (status: StepperItemStatus, index: number) => {
+    switch (status) {
+      case StepperItemStatus.todo:
+        return index < items.length - 1 ? <div className={styles.lineDiv}></div> : <></>
+      case StepperItemStatus.focused:
+        return <div className={styles.lineDiv}></div>
+      case StepperItemStatus.done:
+        return <div className={styles.lineDivDone}></div>
+    }
+  }
+
   return (
     <div>
       {items.map((item, index) => (
         <Fragment key={index}>
           <div className={styles.row}>
-            <div className={item.isDone ? styles.circleDone : styles.circle}></div>
-            <p className={item.isDone ? styles.labelDone : styles.label}>{item.label}</p>
+            <div className={getCircleClass(item.status)}></div>
+            <p className={getLabelClass(item.status)}>{item.label}</p>
           </div>
-          {index !== items.length - 1 && <div className={item.isDone ? styles.lineDivDone : styles.lineDiv}></div>}
+          {getLineElement(item.status, index)}
         </Fragment>
       ))}
     </div>
