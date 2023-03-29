18cd1d8dbf4e77776c7c10bf837192c0235fd544
petr-jilek <petr.jilek156@gmail.com>
2023-03-29 22:54:31 +0200

    Edit code

--- src/components/general/Spacer.tsx
+++ src/components/general/Spacer.tsx
@@ -1,9 +1,11 @@
 export interface Props {
   horizontal?: boolean
+  both?: boolean
   size?: number
 }
 
-export default function Spacer({ horizontal = false, size = 10 }: Props) {
+export default function Spacer({ horizontal = false, both = false, size = 10 }: Props) {
+  if (both) return <div style={{ height: `${size}px`, width: `${size}px` }}></div>
   if (horizontal) return <div style={{ height: "100%", width: `${size}px` }}></div>
 
   return <div style={{ width: "100%", height: `${size}px` }}></div>
