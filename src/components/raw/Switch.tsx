ad86e24e77862adfd1d7fdb216d223eb6efdb984
petr-jilek <petr.jilek156@gmail.com>
2023-04-07 17:17:22 +0200

    Edit code

--- src/components/raw/Switch.tsx
+++ src/components/raw/Switch.tsx
@@ -3,15 +3,15 @@ import styles from "./Switch.module.css"
 export interface Props {
   value: boolean
   onChange?: () => void
-  diabled?: boolean
+  disabled?: boolean
 }
 
-export default function Switch({ value, onChange = () => {}, diabled = false }: Props) {
+export default function Switch({ value, onChange = () => {}, disabled = false }: Props) {
   return (
     <label className={styles.component}>
       <input type="checkbox" checked={value} onChange={() => onChange()} />
       <span
-        className={styles.slider + " " + styles.round + " " + (diabled ? styles.sliderDisabled : styles.sliderDefault)}
+        className={styles.slider + " " + styles.round + " " + (disabled ? styles.sliderDisabled : styles.sliderDefault)}
       ></span>
     </label>
   )
