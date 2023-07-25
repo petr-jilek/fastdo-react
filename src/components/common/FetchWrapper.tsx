425c2570ee623fda65a52d3539cf68127c0de4be
petr-jilek <petr.jilek156@gmail.com>
2023-07-25 12:42:29 +0200

    Edit code

--- src/components/common/FetchWrapper.tsx
+++ src/components/common/FetchWrapper.tsx
@@ -7,6 +7,7 @@ export interface Props<T> {
   loading?: () => any
   error?: (load: () => Promise<void>, error?: ErrorModel) => any
   success: (load: () => Promise<void>, value: T) => any
+  onSuccess?: (value: T) => void
   onLoad: () => Promise<ApiResponse<T>>
 }
 
@@ -14,12 +15,16 @@ export default function FetchWrapper<T>({
   loading = () => <CircularProgressPage />,
   error = undefined,
   success,
+  onSuccess = () => {},
   onLoad
 }: Props<T>) {
   const { model, load } = useFetch<T>({ onLoad })
 
   if (model.loading) return loading()
-  if (model.response?.success && model.response.value) return success(load, model.response.value)
+  if (model.response?.success && model.response.value) {
+    onSuccess(model.response.value)
+    return success(load, model.response.value)
+  }
   if (error) error(load, model.response?.error)
   return <FetchError error={model.response?.error} load={load} />
 }
