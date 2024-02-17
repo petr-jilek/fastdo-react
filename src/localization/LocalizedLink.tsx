b32137d856cce7ef1722f7c8fc072231f411cc8a
Petr Jilek <petr.jilek156@gmail.com>
2024-02-17 10:27:45 +0100

    Edit LocalizedLink

--- src/localization/LocalizedLink.tsx
+++ src/localization/LocalizedLink.tsx
@@ -2,6 +2,10 @@ import { Link, LinkProps } from 'react-router-dom'
 
 import { buildLocalizedPath } from './localization'
 
-export default function LocalizedLink(props: LinkProps) {
+export interface Props extends Omit<LinkProps, 'lang'> {
+  lang?: string | null
+}
+
+export default function LocalizedLink({ lang = null, ...props }: Props) {
   return <Link {...props} to={buildLocalizedPath(props.to as string)}></Link>
 }
